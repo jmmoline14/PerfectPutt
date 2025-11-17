993c9032ae50dd739db54a642143f0ab1d0e5b5e
jmmoline14 <jmmoline14@gmail.com>
2025-11-16 21:27:57 -0500

    I updated the device connectivity and made it so that I could interface with BLE

--- device/src/main.cpp
+++ device/src/main.cpp
@@ -1,11 +1,13 @@
+#include <Arduino.h>
 #include "camera.h"
-
+#include "wire.h"
+#include "ArduinoBLE.h"
 /*-----Uncomment the library and class for your specific hardware-----*/
 //#include "himax.h"  // API to read from the Himax camera found on the Portenta Vision Shield Rev.1
 //HM01B0 himax;
 
-#include "hm0360.h" // API to read from the Himax camera found on the Portenta Vision Shield Rev.2
-HM0360 himax;
+#include "himax.h" // API to read from the Himax camera found on the Portenta Vision Shield Rev.2
+HM01B0 himax;
 
 Camera cam(himax);
 #define IMAGE_MODE CAMERA_GRAYSCALE
@@ -13,19 +15,49 @@ FrameBuffer fb(320,240,2);
 
 unsigned long lastUpdate = 0;
 
+//lets right some BLE code
+// helpfull arduino DOC: https://docs.arduino.cc/tutorials/portenta-h7/ble-connectivity/
+BLEService ledService("19b10000-e8f2-537e-4f6c-d104768a1214");
+BLEByteCharacteristic switchCharacteristic("19b10000-e8f2-537e-4f6c-d104768a1214", BLERead | BLEWrite);
+
+
 void setup() {
   Serial.begin(250000);
   //Init the cam QVGA, 30FPS
   cam.begin(CAMERA_R320x240, IMAGE_MODE, 30);
+  //begin the BLE
+  if(!BLE.begin()){
+    while(1){
+      Serial.println("starting BLE failed!");
+      delay(1000);
+    }
+  }
+    // don't continue
+    //set the name and the service
+  BLE.setLocalName("LED-Portenta-01");
+  BLE.setAdvertisedService(ledService);
+  // Add the characteristic to the service
+  ledService.addCharacteristic(switchCharacteristic);
+
+  // Add service
+  BLE.addService(ledService);
+
+  // Set the initial value for the characeristic:
+  switchCharacteristic.writeValue(0);
+
+  // start advertising
+  BLE.advertise();
+  digitalWrite(LEDB, LOW);
+  delay(1000);
+  digitalWrite(LEDB, HIGH);
+  Serial.println("BLE LED Control ready");
+
 }
 
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(!Serial) {    
-    Serial.begin(250000);
-    while(!Serial);
-  }
-  
+  Serial.println("Everything initialized smoothly");
   // Time out after 2 seconds and send new data
   bool timeoutDetected = millis() - lastUpdate > 2000;
 
@@ -39,4 +71,7 @@ void loop() {
   if (cam.grabFrame(fb, 3000) == 0) {
     Serial.write(fb.getBuffer(), cam.frameSize());
   }
+
+
 }
+  
