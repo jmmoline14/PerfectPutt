4778a31612bd4afe32849ce800471989912692a7
jmmoline14 <jmmoline14@gmail.com>
2025-11-19 10:37:08 -0500

    added the data protocol and made some placeholder data so we could test

--- device/src/main.cpp
+++ device/src/main.cpp
@@ -18,7 +18,11 @@ unsigned long lastUpdate = 0;
 //lets right some BLE code
 // helpfull arduino DOC: https://docs.arduino.cc/tutorials/portenta-h7/ble-connectivity/
 BLEService ledService("19b10000-e8f2-537e-4f6c-d104768a1214");
-BLEByteCharacteristic switchCharacteristic("19b10000-e8f2-537e-4f6c-d104768a1214", BLERead | BLEWrite);
+BLECharacteristic switchCharacteristic(
+  "19b10001-e8f2-537e-4f6c-d104768a1214",
+  BLERead | BLENotify | BLEWrite,
+  20    // max length in bytes
+);
 
 
 void setup() {
@@ -43,7 +47,7 @@ void setup() {
   BLE.addService(ledService);
 
   // Set the initial value for the characeristic:
-  switchCharacteristic.writeValue(0);
+  switchCharacteristic.writeValue((uint8_t)0);
 
   // start advertising
   BLE.advertise();
@@ -57,10 +61,31 @@ void setup() {
 
 
 void loop() {
+  //lets construct our BLE data
+  uint8_t packet[15];
+  packet[0] = 0x00; //protocol byte
+  packet[1] = 0x01; // high byte x acceleration
+  packet[2] = 0x02; // low byte x acceleration
+  packet[3] = 0x03; // High byte y acceleration
+  packet[4] = 0x04; // low byte y acceleration
+  packet[5] = 0x05; // high byte z acceleration
+  packet[6] = 0x06; // low byte z acceleration
+  packet[7] = 0x07; // high byte gyro x acceleration
+  packet[8] = 0x08; // low byte gyro x acceleration
+  packet[9] = 0x09; // high byte gyro y acceleration
+  packet[10] = 0x0A; // low byte gyro y acceleration
+  packet[11] = 0x0B; // high byte gyro z acceleration
+  packet[12] = 0x0C; // low byte gyro z acceleration
+  packet[13] = 0x0D; // FSR 1
+  packet[14] = 0x0E; // FSR 2
+  // Listen for Bluetooth® Low Energy peripherals to connect:
+  BLEDevice central = BLE.central();
   Serial.println("Everything initialized smoothly");
   // Time out after 2 seconds and send new data
-  bool timeoutDetected = millis() - lastUpdate > 2000;
-
+  if(central){
+    switchCharacteristic.writeValue(packet, sizeof(packet));
+  }
+  bool timeoutDetected = (millis() - lastUpdate > 2000);
   // Wait until the receiver acknowledges
   // that they are ready to receive new data
   if(!timeoutDetected && Serial.read() != 1) return;
