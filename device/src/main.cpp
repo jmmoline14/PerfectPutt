770136515495108d7f6f717bf899c4646695d725
jmmoline14 <jmmoline14@gmail.com>
2026-01-28 15:54:15 -0500

    Bluetooth capabilities functional

--- device/src/main.cpp
+++ device/src/main.cpp
@@ -1,66 +1,105 @@
 #include <Arduino.h>
-#include "camera.h"
-#include "wire.h"
 #include "ArduinoBLE.h"
+//Adafruit_BNO055 bno = Adafruit_BNO055(55);
 /*-----Uncomment the library and class for your specific hardware-----*/
 //#include "himax.h"  // API to read from the Himax camera found on the Portenta Vision Shield Rev.1
 //HM01B0 himax;
 
 #include "himax.h" // API to read from the Himax camera found on the Portenta Vision Shield Rev.2
 HM01B0 himax;
-
+bool scanning = false;
 Camera cam(himax);
 #define IMAGE_MODE CAMERA_GRAYSCALE
 FrameBuffer fb(320,240,2);
 
 unsigned long lastUpdate = 0;
-
+uint8_t packet[25];
+BLEDevice esp32; 
 //lets right some BLE code
 // helpfull arduino DOC: https://docs.arduino.cc/tutorials/portenta-h7/ble-connectivity/
-BLEService ledService("19b10000-e8f2-537e-4f6c-d104768a1214");
-BLECharacteristic switchCharacteristic(
+BLEService androidService("19b10000-e8f2-537e-4f6c-d104768a1214");
+BLECharacteristic androidCharacteristic(
   "19b10001-e8f2-537e-4f6c-d104768a1214",
-  BLERead | BLENotify | BLEWrite,
-  20    // max length in bytes
+  BLERead | BLENotify,
+  27    // max length in bytes
 );
 
+void central() {
 
+  if (!esp32 || !esp32.connected()) {
+    esp32 = BLE.available();
+    if (esp32 && esp32.localName() == "ESP32PERFECTPUTT") {
+      BLE.stopScan();
+      scanning = false;
+      if (esp32.connect()) {
+        esp32.discoverAttributes();
+      }
+    } else {
+
+      if(!scanning){
+        BLE.scan(); 
+        scanning = true;
+      }
+    }
+  } else {
+
+    BLECharacteristic myChar = esp32.characteristic("d8450001-6421-4f80-928d-19548483b890");
+    if (myChar && myChar.canRead()) {
+      myChar.readValue(packet, 25); 
+      Serial.print(packet[3]);
+    }
+    else {
+
+      esp32.discoverAttributes();
+    }
+  }
+}
+void peripheral() {
+  BLEDevice central = BLE.central();
+
+  if (central && central.connected()) {
+
+    androidCharacteristic.writeValue(packet, 25);
+  }
+
+  bool timeoutDetected = (millis() - lastUpdate > 2000);
+
+  lastUpdate = millis();
+}
 void setup() {
   Serial.begin(250000);
-  //Init the cam QVGA, 30FPS
+
   cam.begin(CAMERA_R320x240, IMAGE_MODE, 30);
-  //begin the BLE
+
   if(!BLE.begin()){
     while(1){
       Serial.println("starting BLE failed!");
       delay(1000);
     }
   }
-    // don't continue
-    //set the name and the service
-  BLE.setLocalName("LED-Portenta-01");
-  BLE.setAdvertisedService(ledService);
-  // Add the characteristic to the service
-  ledService.addCharacteristic(switchCharacteristic);
+  BLE.setLocalName("PerfectPuttPortenta");
+  BLE.setAdvertisedService(androidService); // add the service UUID
+  androidService.addCharacteristic(androidCharacteristic); // add the android characteristic
+  BLE.addService(androidService); // Add the battery service
 
-  // Add service
-  BLE.addService(ledService);
-
-  // Set the initial value for the characeristic:
-  switchCharacteristic.writeValue((uint8_t)0);
-
-  // start advertising
+  // Start advertising
   BLE.advertise();
-  digitalWrite(LEDB, LOW);
-  delay(1000);
-  digitalWrite(LEDB, HIGH);
-  Serial.println("BLE LED Control ready");
+
+  BLE.scan();
 
 }
 
 
 
 void loop() {
+  static bool Receive = true;
+  static uint8_t frameID = 0;
+   // Grab frame and write to serial
+   /*
+  if (cam.grabFrame(fb, 3000) == 0) {
+    Serial.write(fb.getBuffer(), cam.frameSize());
+  }*/
+  /*
   //lets construct our BLE data
   uint8_t packet[15];
   packet[0] = 0x00; //protocol byte
@@ -78,25 +117,13 @@ void loop() {
   packet[12] = 0x0C; // low byte gyro z acceleration
   packet[13] = 0x0D; // FSR 1
   packet[14] = 0x0E; // FSR 2
+  */
   // Listen for Bluetooth® Low Energy peripherals to connect:
-  BLEDevice central = BLE.central();
-  Serial.println("Everything initialized smoothly");
-  // Time out after 2 seconds and send new data
-  if(central){
-    switchCharacteristic.writeValue(packet, sizeof(packet));
-  }
-  bool timeoutDetected = (millis() - lastUpdate > 2000);
-  // Wait until the receiver acknowledges
-  // that they are ready to receive new data
-  if(!timeoutDetected && Serial.read() != 1) return;
+  // connect to the ESP32
 
-  lastUpdate = millis();
+  central();
+  peripheral();
+
   
-  // Grab frame and write to serial
-  if (cam.grabFrame(fb, 3000) == 0) {
-    Serial.write(fb.getBuffer(), cam.frameSize());
-  }
-
-
 }
   
