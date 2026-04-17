64b3ffd191c3a08e04c4ad33511529ee7aa83bd1
dueling12 <noahthompson@ufl.edu>
2026-04-16 20:44:58 -0400

    Update Solenoid Script
    New solenoid code

--- Solenoid Control/solenoid_control/solenoid_control.ino
+++ Solenoid Control/solenoid_control/solenoid_control.ino
@@ -1,17 +1,23 @@
 int inval = 0;
 void setup() {
   // put your setup code here, to run once:
-  pinMode(25,OUTPUT);
-  digitalWrite(solenoidPin, LOW);
-  Serial.begin(9600);
+  pinMode(23,OUTPUT);
+  digitalWrite(23, HIGH);
+  Serial.begin(115200);
+  Serial.println("COMM STARTED");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //Values passed to ESP32 must be in range of 0-20
+  //Values passed to ESP32 must be in range of 0-10
   if(Serial.available()>0){
     inval = Serial.parseInt();
-    inval = map(inval, 0, 20, 0, 255);
-    digitalWrite(25,inval);
+    inval = map(inval, 0, 10, 255, 0);
+    digitalWrite(23,inval);
+    Serial.print(inval);
+    delay(5000);
+  }
+  else{
+    digitalWrite(23,HIGH);
   }
 }
